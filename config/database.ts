f3b92d92374ec0033611eec688100d5955e20534
Hoàng Nam <bapnhn1810@gmail.com>
2025-10-04 10:47:22 +0700

    upd conf

--- config/database.ts
+++ config/database.ts
@@ -1,43 +1,21 @@
 import path from 'path';
 
 export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'mysql'); // bắt buộc mysql nếu dùng Railway
-
-  const connections = {
-    mysql: {
+  return {
+    connection: {
+      client: 'mysql',
       connection: {
-        host: env('DATABASE_HOST'),       // từ .env
-        port: env.int('DATABASE_PORT'),   // từ .env, ví dụ 35579
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
         database: env('DATABASE_NAME'),
         user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
-        ssl: {
-          rejectUnauthorized: false,     // cho phép self-signed certificate
-        },
+        ssl: { rejectUnauthorized: false }, // fix self-signed
       },
       pool: {
         min: env.int('DATABASE_POOL_MIN', 2),
         max: env.int('DATABASE_POOL_MAX', 10),
       },
-    },
-
-    sqlite: {
-      connection: {
-        filename: path.join(
-          __dirname,
-          '..',
-          '..',
-          env('DATABASE_FILENAME', '.tmp/data.db')
-        ),
-      },
-      useNullAsDefault: true,
-    },
-  };
-
-  return {
-    connection: {
-      client,
-      ...connections[client],
       acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
     },
   };
